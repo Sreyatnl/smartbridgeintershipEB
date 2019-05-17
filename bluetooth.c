49775f890c4a6769016474b3455a5a3601acc50a
Sreyatnl <50613114+Sreyatnl@users.noreply.github.com>
2019-05-17 09:57:04 +0530

    Add files via upload

---
+++ bluetooth.c
@@ -0,0 +1,60 @@
+/*
+ * bluetooth1.c
+ *
+ * Created: 16-05-2019 11:45:58
+ * Author : SUNDARI
+ */ 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <avr/io.h>
+#include <inttypes.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define LED PORTB
+char buffer[10];
+
+void USARTInit (uint16_t ubrr_value)
+{
+	UBRRL = ubrr_value;
+	UBRRH = (ubrr_value>>8);
+	UCSRC=(1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);
+	UCSRB=(1<<RXEN)|(1<<TXEN);
+}
+char USARTReadChar()
+{
+	while(!(UCSRA&(1<<RXC)))
+	{
+		
+	}
+	return UDR;
+}
+void USARTWriteChar(char data)
+{
+	while(!(UCSRA&(1<<UDRE)))
+	{
+		
+	}
+	UDR=data;
+}
+int main(void)
+{
+	USARTInit(77);
+	DDRB=0XFF;
+	char data;
+	LED =0;
+    /* Replace with your application code */
+    while (1) 
+    {
+		data=USARTReadChar();
+		if(data == '1')
+		{
+			LED|=0XFF;
+    }
+	else if(data =='2')
+	{
+		LED = 0X00;
+}
+	}
+	return 0;
+}
---
+++ servo motor.c
@@ -0,0 +1,34 @@
+/*
+ * servo motor.c
+ *
+ * Created: 16-05-2019 15:01:33
+ * Author : SUNDARI
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16000000UL
+
+
+int main(void)
+{
+	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);
+	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10);
+	ICR1=4999;
+	DDRD|=(1<<PD5);
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		OCR1A=97;
+		_delay_ms(1000);
+		OCR1A=280;
+		_delay_ms(1000);
+		OCR1A=535;
+		_delay_ms(1000);
+		
+		
+		
+    }
+}
+
